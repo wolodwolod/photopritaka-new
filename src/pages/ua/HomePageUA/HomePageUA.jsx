2d34a385a551686aa602a03055fcdc5a2411930e
Volodymyr Bendyk <vvbendyk@gmail.com>
2023-01-03 23:17:48 +0200

    8

---
+++ src/pages/ua/HomePageUA/HomePageUA.jsx
@@ -0,0 +1,13 @@
+import NavbarUA from '../../../client/ua/NavbarUA';
+
+const HomePageUA = () => {
+  return (
+    <main>
+      <div className="container">
+        <NavbarUA />
+      </div>
+    </main>
+  );
+};
+
+export default HomePageUA;
