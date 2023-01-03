046089b3016f0dab818514529d8737aacefe2860
Volodymyr Bendyk <vvbendyk@gmail.com>
2023-01-03 16:43:52 +0200

    5

--- src/client/ua/Navbar/Navbar.jsx
+++ src/client/ua/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 // import { NavLink } from "react-router-dom";
 
-// import s from "./navbar.module.scss"
+import s from "./navbar.module.scss"
 
 import camera from '../../../shared/icons/camera2.svg';
 import iconsUKR from '../../../shared/icons/icons-UKR.png';
@@ -13,22 +13,22 @@ const Navbar = () => {
     return (
 
       <header>
-        <div class="container">
-          <div class="row">
-            <nav class="navbar navbar-expand-lg nav">
-              <div class="container-fluid bold">
-                <a class="navbar-brand logo" href="/"
+        <div className="container">
+          <div className="row">
+            <nav className="navbar navbar-expand-lg nav">
+              <div className="container-fluid bold">
+                <a className="navbar-brand s.logo" href="/"
                 ><img
                     src={camera}
                     alt="Logo"
                     width="38"
                     height="24"
-                    class="d-inline-block align-text-top"
-                  />PRITAKA&nbsp;<span class="yellow">PHOTO</span></a
+                    className="d-inline-block align-text-top"
+                  />PRITAKA&nbsp;<span className={s.yellow}>PHOTO</span></a
                 >
 
                 <button
-                  class="navbar-toggler"
+                  className="navbar-toggler"
                   type="button"
                   data-bs-toggle="collapse"
                   data-bs-target="#navbarNavDropdown"
@@ -36,29 +36,29 @@ const Navbar = () => {
                   aria-expanded="false"
                   aria-label="Toggle navigation"
                 >
-                  <span class="navbar-toggler-icon"></span>
+                  <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                  <ul class="navbar-nav nav text-uppercase">
-                    <li class="nav-item">
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                  <ul className="navbar-nav nav text-uppercase">
+                    <li className="nav-item">
                       <a
-                        class="nav-link active"
+                        className="nav-link active"
                         aria-current="page"
                         href="./index.html"
                       >Головна</a
                       >
                     </li>
-                    <li class="nav-item">
-                      <a class="nav-link text-uppercase" href="/"
+                    <li className="nav-item">
+                      <a className="nav-link text-uppercase" href="/"
                       >Фотограф</a
                       >
                     </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="/">Контакти</a>
+                    <li className="nav-item">
+                      <a className="nav-link" href="/">Контакти</a>
                     </li>
-                    <li class="nav-item mr-50 dropdown">
+                    <li className="nav-item mr-50 dropdown">
                       <a
-                        class="nav-link dropdown-toggle"
+                        className="nav-link dropdown-toggle"
                         href="/"
                         role="button"
                         data-bs-toggle="dropdown"
@@ -66,31 +66,31 @@ const Navbar = () => {
                       >
                         Фотосесії
                       </a>
-                      <ul class="dropdown-menu">
+                      <ul className="dropdown-menu">
                         <li>
-                          <a class="dropdown-item" href="/"
+                          <a className="dropdown-item" href="/"
                           >Сімейне фото</a
                           >
                         </li>
                         <li>
-                          <a class="dropdown-item" href="/"
+                          <a className="dropdown-item" href="/"
                           >Романтичне фото</a
                           >
                         </li>
                         <li>
-                          <a class="dropdown-item" href="/"
+                          <a className="dropdown-item" href="/"
                           >Новонароджені</a
                           >
                         </li>
                         <li>
-                          <a class="dropdown-item" href="/">Події</a>
+                          <a className="dropdown-item" href="/">Події</a>
                         </li>
                       </ul>
                     </li>
-                    <li class="nav-item">
-                      <div class="dropdown">
+                    <li className="nav-item">
+                      <div className="dropdown">
                         <a
-                          class="dropdown-toggle"
+                          className="dropdown-toggle"
                           href="/"
                           role="button"
                           data-bs-toggle="dropdown"
@@ -100,29 +100,29 @@ const Navbar = () => {
                             alt="Logo"
                             width="35"
                             height="30"
-                            class="d-inline-block align-text-top"
+                            className="d-inline-block align-text-top"
                           /></a>
 
-                        <ul class="dropdown-menu fs-14">
+                        <ul className="dropdown-menu fs-14">
                           <li>
-                            <a class="dropdown-item" href="/"
+                            <a className="dropdown-item" href="/"
                             ><img
                                 src={iconsGB}
                                 alt="Logo"
                                 width="35"
                                 height="30"
-                                class="d-inline-block align-text-center"
+                                className="d-inline-block align-text-center"
                               />&nbsp;&nbsp;English</a
                             >
                           </li>
                           <li>
-                            <a class="dropdown-item" href="/"
+                            <a className="dropdown-item" href="/"
                             ><img
                                 src={iconsFR}
                                 alt="Logo"
                                 width="35"
                                 height="30"
-                                class="d-inline-block align-text-center"
+                                className="d-inline-block align-text-center"
                               />&nbsp;&nbsp;Français</a
                             >
                           </li>
@@ -140,6 +140,6 @@ const Navbar = () => {
        
     )
 
-}
+};
 
 export default Navbar;
