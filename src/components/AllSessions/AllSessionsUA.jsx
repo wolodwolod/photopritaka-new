2739a1f5b72bcb582bb687d472fe095a78e68dff
Volodymyr Bendyk <vvbendyk@gmail.com>
2023-01-07 23:07:40 +0200

    Update AllSessionsUA.jsx

--- src/components/AllSessions/AllSessionsUA.jsx
+++ src/components/AllSessions/AllSessionsUA.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './allsessions.scss';
 
 const AllSessionsUA = () => {
@@ -7,33 +8,33 @@ const AllSessionsUA = () => {
     <section className="all-sessions">
       <div className="container">
         <div className="row">
-          <div className="col-md-6 col-sm-12">
-            <a href="/ua/family" className="session text-uppercase">
-              <div className="session-box">
+          <div className="col-md-6 col-sm-12 text-uppercase">
+            <div className="session session-box">
+              <Link to="/ua/family" relative="path">
                 <h2 className="text-center">Сімейне фото</h2>
-              </div>
-            </a>
+              </Link>
+            </div>
           </div>
-          <div className="col-md-6 col-sm-12">
-            <a href="/ua/romantic" className="session text-uppercase">
-              <div className="session-box">
+          <div className="col-md-6 col-sm-12 text-uppercase">
+            <div className="session session-box">
+              <Link to="/ua/romantic" relative="path">
                 <h2 className="text-center">Романтичне фото</h2>
-              </div>
-            </a>
+              </Link>
+            </div>
           </div>
-          <div className="col-md-6 col-sm-12">
-            <a href="/ua/newborns" className="session text-uppercase">
-              <div className="session-box">
+          <div className="col-md-6 col-sm-12 text-uppercase">
+            <div href="/ua/newborns" className="session session-box">
+              <Link to="/ua/newborns" relative="path">
                 <h2 className="text-center">НОВОНАРОДЖЕНІ</h2>
-              </div>
-            </a>
+              </Link>
+            </div>
           </div>
-          <div className="col-md-6 col-sm-12">
-            <a href="/ua/events" className="session text-uppercase">
-              <div className="session-box">
+          <div className="col-md-6 col-sm-12 text-uppercase">
+            <div href="/ua/events" className="session session-box">
+              <Link to="/ua/events" relative="path">
                 <h2 className="text-center">ПОДІЇ</h2>
-              </div>
-            </a>
+              </Link>             
+            </div>
           </div>
         </div>
       </div>
