b4a99dae6745f3b4f67f9cc1811ab4cb53efc09b
Volodymyr Bendyk <vvbendyk@gmail.com>
2023-01-12 18:48:54 +0200

    send mes env

--- src/shared/services/sendMessageUA.js
+++ src/shared/services/sendMessageUA.js
@@ -1,35 +1,28 @@
 import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
 
-// import React from "react";
-// import env from "react-dotenv";
-// import * as dotenv from 'dotenv';
-// dotenv.config();
-// require('dotenv').config()
-// console.log(process.env) // remove this after you've confirmed it is working
+const sendMessage = payload => {
+  const { email, name, text } = payload;
 
-const sendMessage = (payload) => {
+  // const example = process.env.REACT_APP_EXAMPLE;
+  // console.log(process.env);
+  // console.log(process.env.NODE_ENV);
+  // console.log(email, name, text);
+  // console.log(example);
 
-  const {email, name, text} = payload;
-
-
-  console.log(process.env)
-  console.log(process.env.NODE_ENV)
-  console.log(email, name, text)
-  console.log(process.env.REACT_APP_EXAMPLE)
   
-  
+  const REGION = process.env.REACT_APP_REGION;
+
   const CREDENTIAL = {
     accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
     secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
   };
-  //   const REGION = process.env.REGION;
-  // const ses = new AWS.SES({ region: REGION, credentials: CREDENTIAL });
+  
   const HTML_CONTENTS =
     '<html>' +
     '<h2>Повідомлення фотографу від користувача сайту PritakaPhoto</h2>' +
     `<table style='border: 1px solid #333'><tbody><tr><td style='border: 1px solid #333; padding: 1px 5px'>Ім'я</td><td style='border: 1px solid #333; padding: 1px 5px'>${name}</td></tr><tr><td style='border: 1px solid #333; padding: 1px 5px'>Електронна адреса</td><td style='border: 1px solid #333; padding: 1px 5px'>${email}</td></tr><tr><td style='border: 1px solid #333; padding: 1px 5px'>Повідомлення</td><td style='border: 1px solid #333; padding: 1px 5px'>${text}</td></tr></tbody></table >` +
     '</html>';
-    
+
   const params = {
     Destination: {
       ToAddresses: [process.env.REACT_APP_TO_MAIL_ADDRESS],
@@ -52,18 +45,10 @@ const sendMessage = (payload) => {
   };
 
   const sesClient = new SESClient({
-    region: process.env.REACT_APP_REGION,
+    region: REGION,
     credentials: CREDENTIAL,
   });
-
-  // ses.sendEmail(params, function (err, data) {
-  //   if (err) {
-  //     console.log("unable to send email", err);
-  //   } else {
-  //     console.log("email is send", data);
-  //   }
-  // });
-
+  
   const run = async () => {
     try {
       const data = await sesClient.send(new SendEmailCommand(params));
