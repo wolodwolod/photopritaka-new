d5bc7df289ce401445f7469040e09040cb29fffa
Volodymyr Bendyk <vvbendyk@gmail.com>
2023-01-12 19:32:01 +0200

    Update sendMessageUA.js

--- src/shared/services/sendMessageUA.js
+++ src/shared/services/sendMessageUA.js
@@ -3,11 +3,11 @@ import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
 const sendMessage = payload => {
   const { email, name, text } = payload;
 
-  // const example = process.env.REACT_APP_EXAMPLE;
+  const example = process.env.REACT_APP_EXAMPLE;
   // console.log(process.env);
   // console.log(process.env.NODE_ENV);
   // console.log(email, name, text);
-  // console.log(example);
+  console.log(example);
 
   
   const REGION = process.env.REACT_APP_REGION;
