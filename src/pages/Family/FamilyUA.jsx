11373bb16229b3236d963f08ded7860d2f801a37
Volodymyr Bendyk <vvbendyk@gmail.com>
2023-01-17 20:57:30 +0200

    family

--- src/pages/Family/FamilyUA.jsx
+++ src/pages/Family/FamilyUA.jsx
@@ -1,11 +1,18 @@
+// import arrayOfImg from '../../shared/images/array';
+import ImgFamilyGallery from 'components/ImageGallery/ImgFamilyGallery';
 
 
 const FamilyUA = () => {
   return (
     <main>
       <div>
+        <div className="col-12">
+          <h2 className="text-center text-uppercase color3 mt-5">
+            Сімейна фотосесія
+          </h2>
+        </div>
+        <ImgFamilyGallery  />
         
-        <h2>Сімейні</h2>
       </div>
     </main>
   );
