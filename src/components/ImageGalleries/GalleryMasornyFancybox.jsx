74974f41f2183f7917e10dc6c5cef47bba66e6f9
Volodymyr Bendyk <vvbendyk@gmail.com>
2023-01-20 20:19:16 +0200

    Update GalleryMasornyFancybox.jsx

--- src/components/ImageGalleries/GalleryMasornyFancybox.jsx
+++ src/components/ImageGalleries/GalleryMasornyFancybox.jsx
@@ -1,33 +1,11 @@
-// import { useState, useCallback } from 'react';
-// import Masonry from 'react-masonry-css';
+
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 import Fancybox from '../../shared/services/fancybox';
-// import Modal from 'components/Modal';
+
 import s from './image-gallery.module.scss';
 import './masonry.scss';
 
-const Gallery = ({ imgs }) => {
-  //   const [modal, setModal] = useState({
-  //     open: false,
-  //     lImg: null,
-  //     tag: '',
-  //   });
-
-  //   const openModal = useCallback((limg, tag) => {
-  //     setModal({
-  //       open: true,
-  //       lImg: limg,
-  //       tag: { tag },
-  //     });
-  //   }, []);
-
-  //   const closeModal = () => {
-  //     setModal({
-  //       open: false,
-  //       lImg: null,
-  //       tag: '',
-  //     });
-  //   };
+const Gallery = ({ imgs }) => { 
 
   console.log(imgs);
 
@@ -36,10 +14,7 @@ const Gallery = ({ imgs }) => {
       <div
         data-fancybox="gallery"
         data-src={limg}
-        key={id}
-        // onClick={() => {
-        //   openModal(limg, tag);
-        // }}
+        key={id}        
       >
         <img className={s.GalleryItem__Img} src={img} alt={tag} />
       </div>
@@ -49,16 +24,10 @@ const Gallery = ({ imgs }) => {
   return (
     <div className="container">
       <Fancybox>
-        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 800: 2, 900: 3 }}>
           <Masonry gutter={'10px'}>{imgsList}</Masonry>
         </ResponsiveMasonry>
-      </Fancybox>
-
-      {/* {modal.open && (
-        <Modal onClose={closeModal}>
-          <img className={s.Modal_Img} src={modal.lImg} alt={modal.tag} />
-        </Modal>
-      )} */}
+      </Fancybox>      
     </div>
   );
 };
