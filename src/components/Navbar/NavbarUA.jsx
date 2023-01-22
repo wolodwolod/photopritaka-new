28807fc008605b0a9895c55be3f45637acc8b324
Volodymyr Bendyk <vvbendyk@gmail.com>
2023-01-22 13:33:49 +0200

    menu

--- src/components/Navbar/NavbarUA.jsx
+++ src/components/Navbar/NavbarUA.jsx
@@ -3,13 +3,17 @@ import { NavLink } from 'react-router-dom';
 import './navbar.scss';
 import styles from './navbar.module.scss';
 
+
 import camera from '../../shared/icons/camera2.svg';
 import iconsUKR from '../../shared/icons/icons-UKR.png';
 import iconsGB from '../../shared/icons/icons-GB.png';
 import iconsFR from '../../shared/icons/icons-FR.png';
 
+
 const getActiveClass = ({ isActive }) =>
   isActive ? styles.linkActive : styles.link;
+
+  
 
 const NavbarUA = () => {
   return (
@@ -17,7 +21,8 @@ const NavbarUA = () => {
       <div className="container">
         <div className="row">
           <nav className="navbar navbar-expand-lg nav">
-            <div className="container-fluid bold">
+
+            <div className="container-fluid  bold">
               <div className="navbar-brand logo">
                 <NavLink to="/" className={getActiveClass}>
                   <img
@@ -42,8 +47,9 @@ const NavbarUA = () => {
               >
                 <span className="navbar-toggler-icon"></span>
               </button>
-              <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav nav text-uppercase">
+
+              <div className="collapse navbar-collapse menu-wrapper" id="navbarNavDropdown">
+                <ul className="navbar-nav nav">
                   <li className="nav-item nav-link">
                     {/* <a className="nav-link" aria-current="page" href="/"> */}
                     <NavLink to="/" className={getActiveClass}>
@@ -51,34 +57,16 @@ const NavbarUA = () => {
                     </NavLink>
                     {/* </a> */}
                   </li>
-                  <li className="nav-item nav-link">
-                    {/* <a
-                      className="nav-link"
-                      href="/ua/photographer"
-                    > */}
-                    <NavLink to="/ua/photographer" className={getActiveClass}>
-                      Фотограф
-                    </NavLink>
-
-                    {/* </a> */}
-                  </li>
-                  <li className="nav-item nav-link">
-                    {/* <a className="nav-link" href="/ua/contacts"> */}
-                    <NavLink to="/ua/contacts" className={getActiveClass}>
-                      Контакти
-                    </NavLink>
-
-                    {/* </a> */}
-                  </li>
-                  <li className="nav-item mr-50 dropdown">
+                  <li className="nav-item dropdown">
                     <a
                       className="nav-link dropdown-toggle"
                       href="/"
                       role="button"
                       data-bs-toggle="dropdown"
                       aria-expanded="false"
+                      
                     >
-                      Фотосесії
+                      Портфоліо
                     </a>
                     <ul className="dropdown-menu">
                     <li className="dropdown-item">
@@ -119,6 +107,27 @@ const NavbarUA = () => {
                       </li>
                     </ul>
                   </li>
+                  <li className="nav-item nav-link">
+                    {/* <a className="nav-link" href="/ua/contacts"> */}
+                    <NavLink to="/ua/contacts" className={getActiveClass}>
+                      Контакти
+                    </NavLink>
+
+                    {/* </a> */}
+                  </li>
+                  <li className="nav-item nav-link">
+                    {/* <a
+                      className="nav-link"
+                      href="/ua/photographer"
+                    > */}
+                    <NavLink to="/ua/photographer" className={getActiveClass}>
+                      Відгуки
+                    </NavLink>
+
+                    {/* </a> */}
+                  </li>
+                  
+                  
                   <li className="nav-item">
                     <div className="dropdown">
                       <a
