91fc44c04865eb312f77f00e08f2a8f984d4f080
Volodymyr Bendyk <vvbendyk@gmail.com>
2023-01-28 21:15:46 +0200

    Create responses.js

---
+++ src/components/FeedbacksList/responses.js
@@ -0,0 +1,13 @@
+import Volodymyr_Bendyk from '../../shared/images/feedbacks-img/Volodymyr_Bendyk.jpg';
+
+const responsesUA = [
+  {
+    id: 1,
+    name: 'Володимир Бендик',
+    img: Volodymyr_Bendyk,
+    response:
+      "Тетяна здатна підвищити настрій, прибрати скованість перед камерою, створити приємну атмосферу. \n Світлини Тетяни - це можливість побачити чудо в звичайному. Вони сповнені життям і багато чого розровідають не тільки про об'єкти зйомки, але і про самого фотографа."
+  },
+];
+
+export default responsesUA;
