43a83f361ba83400da76b50336c980b3969de808
Volodymyr Bendyk <vvbendyk@gmail.com>
2023-01-28 21:43:47 +0200

    fb

--- src/components/FeedbacksList/FeedbacksListUA.jsx
+++ src/components/FeedbacksList/FeedbacksListUA.jsx
@@ -5,26 +5,33 @@ const FeedbacksListUA = () => {
   return (
     <div className="feedbacks_list">
       <div className="feedbacks_item mt-3">
-        <div className="feedbacks_item-info justify-content-center">
-          <img
-            src={Volodymyr_Bendyk}
-            alt="Володимир Бендик"
-            className="rounded mx-auto d-block"
-          />
-          <p className="text-center mb-0 mt-3">Володимир Бендик</p>
+        <div className="row ">
+        <div className="col-md-2 col-sm-12">
+          <div className="feedbacks_item-info justify-content-center">
+            <img
+              src={Volodymyr_Bendyk}
+              alt="Володимир Бендик"
+              className="rounded mx-auto d-block"
+            />
+            <p className="text-center mb-0 mt-3">Володимир Бендик</p>
+          </div>
         </div>
-        <div className="feedbacks_item-text">
-          <p>
-            Тетяна здатна підвищити настрій, прибрати скованість перед камерою,
-            створити приємну атмосферу.
-          </p>
-          <p>
-            Світлини Тетяни - це можливість побачити чудо в звичайному. Вони
-            сповнені життям і багато чого розровідають не тільки про об'єкти
-            зйомки, але і про самого фотографа.
-          </p>
+        <div className="col-md-10 col-sm-12">
+          <div className="feedbacks_item-text">
+            <p>
+              Тетяна здатна підвищити настрій, прибрати скованість перед
+              камерою, створити приємну атмосферу.
+            </p>
+            <p>
+              Світлини Тетяни - це можливість побачити чудо в звичайному. Вони
+              сповнені життям і багато чого розровідають не тільки про об'єкти
+              зйомки, але і про самого фотографа.
+            </p>
+          </div>
         </div>
       </div>
+      </div>
+      
     </div>
   );
 };
