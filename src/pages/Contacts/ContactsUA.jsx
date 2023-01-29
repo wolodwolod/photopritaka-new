2fa5c41fdd1b6f0d7002d89aaa39b6d43d0a79ca
Volodymyr Bendyk <vvbendyk@gmail.com>
2023-01-29 13:32:32 +0200

    blue-yellow

--- src/pages/Contacts/ContactsUA.jsx
+++ src/pages/Contacts/ContactsUA.jsx
@@ -4,6 +4,8 @@ import MessageFormUA from 'components/MessageForm/MessageFormUA';
 import sendMessageUA from 'shared/services/sendMessageUA';
 
 import photographer from '../../shared/images/common-img/TP-xl-01.jpg';
+import photographer_sm from '../../shared/images/common-img/TP-sm.jpg';
+
 import location_svg from '../../shared/icons/location.svg';
 import envelope_svg from '../../shared/icons/envelope.svg';
 
@@ -16,43 +18,60 @@ const СontactsUA = () => {
         </div> */}
 
         <div className="row">
-          <div className="col-lg-3 col-md-4 col-sm-12 ">
-            <div className="contacts_photo-box">
-              <img
-                src={photographer}
-                alt="Тетяна Притака"
-                className="contacts-img"
-              />
+          <div className="col-lg-6 col-md-12 col-sm-12">
+            <div className="first">
+              <div className="row">
+                <div className="col-lg-6 col-md-5 col-sm-12">
+                  <div className="contacts_photo-box">
+                    <picture>
+                      <source media="(min-width: 992px)" srcSet={photographer} />
+
+                      <img
+                        className="contacts-img"
+                        srcSet={photographer_sm}
+                        alt="Тетяна Притака"
+                        loading="lazy"
+                      />
+
+                      
+                    </picture>
+
+                    {/* <img
+                      src={photographer}
+                      alt="Тетяна Притака"
+                      className="contacts-img"
+                    /> */}
+                  </div>
+                </div>
+                <div className="col-lg-6 col-md-7 col-sm-12 ">
+                  <div className="contacts_info">
+                    <div className="contacts_info_loc">
+                      <img
+                        src={location_svg}
+                        alt="Location"
+                        width="30"
+                        // height="48"
+                        className="d-inline-block align-text-top"
+                      />
+                      <p>місто Нансі, Франція</p>
+                    </div>
+                    <div className="contacts_info_mail">
+                      <img
+                        src={envelope_svg}
+                        alt="E-Mail"
+                        width="25"
+                        // height="48"
+                        className="d-inline-block align-text-top contacts_info_svg"
+                      />
+                      <p>pritakaphoto@gmail.com</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="col-lg-3 col-md-3 col-sm-12">
-            <div className="contacts_info">
-              <div className="contacts_info_loc">
-                <img
-                  src={location_svg}
-                  alt="Location"
-                  width="30"
-                  // height="48"
-                  className="d-inline-block align-text-top"
-                  
-                />
-                <p>місто Нансі, Франція</p>
-              </div>
-              <div className="contacts_info_mail">
-              <img
-                  src={envelope_svg}
-                  alt="E-Mail"
-                  width="25"
-                  // height="48"
-                  className="d-inline-block align-text-top contacts_info_svg"
-                />
-                <p>
-                pritakaphoto@gmail.com
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-6 col-md-5 col-sm-12 ">
+
+          <div className="col-lg-6 col-md-12 col-sm-12 ">
             <div className="contacts_message">
               <MessageFormUA onSubmit={sendMessageUA} />
             </div>
