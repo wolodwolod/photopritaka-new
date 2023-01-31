97ff55ae7250e7def56fce1862781bb01b4f1eb6
Volodymyr Bendyk <vvbendyk@gmail.com>
2023-01-31 14:33:06 +0200

    home

--- src/pages/HomePage/HomePageUA.jsx
+++ src/pages/HomePage/HomePageUA.jsx
@@ -1,49 +1,26 @@
 import '../pages.scss';
 
-// import AllSessionsUA from 'components/AllSessions/AllSessionsUA';
 import Carousel from '../../shared/services/carousel.js';
-// import MainUA from 'components/Main/PhotographerUA';
 
 import img1 from '../../shared/images/portrait-img/s/1.jpg';
-import img2 from '../../shared/images/portrait-img/s/2.jpg';
-import img3 from '../../shared/images/portrait-img/s/3.jpg';
-import img4 from '../../shared/images/portrait-img/s/4.jpg';
-import img5 from '../../shared/images/portrait-img/s/5.jpg';
-
-import limg1 from '../../shared/images/portrait-img/l/1.jpg';
-import limg2 from '../../shared/images/portrait-img/l/2.jpg';
-import limg3 from '../../shared/images/portrait-img/l/3.jpg';
-import limg4 from '../../shared/images/portrait-img/l/4.jpg';
-import limg5 from '../../shared/images/portrait-img/l/5.jpg';
+import img2 from '../../shared/images/family-img/s/21.jpg';
+import img3 from '../../shared/images/romantic-img/s/7.jpg';
+import img4 from '../../shared/images/newborns-img/s/1.jpg';
+import img5 from '../../shared/images/events-img/s/20.jpg';
 
 const HomePageUA = () => {
   const slideItems = [
-    { id: 1, img: img1, limg: limg1, tag: 'photo1' },
-    { id: 2, img: img2, limg: limg2, tag: 'photo2' },
-    { id: 3, img: img3, limg: limg3, tag: 'photo3' },
-    { id: 4, img: img4, limg: limg4, tag: 'photo4' },
-    { id: 5, img: img5, limg: limg5, tag: 'photo5' },
+    { id: 1, img: img1, tag: 'Портфоліо | Портрет' },
+    { id: 2, img: img2, tag: 'Портфоліо | Сімейне фото' },
+    { id: 3, img: img3, tag: 'Портфоліо | Романтичне фото' },
+    { id: 4, img: img4, tag: 'Портфоліо | Новонароджені' },
+    { id: 5, img: img5, tag: 'Портфоліо | Події' },
   ];
 
   return (
     <main>
       <div className="container">
-        {/* <div className="row">
-          <div className="col-12">
-            <div className="page_title">
-              <p className="page_about">Всі сесії</p>
-              {/* <h2 className="text-center text-uppercase color3">
-            Це - ти, і ти - ідеал.
-          </h2> */}
-        {/* </div> */}
-        {/* </div> */}
-        {/* </div> */}
-
         <Carousel items={slideItems} options={{ infinite: false }} />
-         
-        
-
-        {/* <AllSessionsUA /> */}
       </div>
     </main>
   );
--- src/shared/services/carousel.js
+++ src/shared/services/carousel.js
@@ -1,11 +1,9 @@
-import "@fancyapps/ui/dist/carousel.css";
+import '@fancyapps/ui/dist/carousel.css';
 
-import React, { useRef, useEffect } from "react";
-import { Carousel as NativeCarousel } from "@fancyapps/ui/dist/carousel.esm.js";
-import { Autoplay } from "@fancyapps/ui/dist/carousel.autoplay.esm.js";
+import React, { useRef, useEffect } from 'react';
+import { Carousel as NativeCarousel } from '@fancyapps/ui/dist/carousel.esm.js';
+import { Autoplay } from '@fancyapps/ui/dist/carousel.autoplay.esm.js';
 NativeCarousel.Plugins.Autoplay = Autoplay;
-
-
 
 function ReactCarousel(props) {
   const wrapper = useRef(null);
@@ -14,11 +12,11 @@ function ReactCarousel(props) {
     const items = props.items || [];
     const opts = props.options || {};
 
-    opts.slides = [...items].map((item) => {
+    opts.slides = [...items].map(item => {
       return { html: item.tag };
     });
 
-    const instance = new NativeCarousel(wrapper.current,  {
+    const instance = new NativeCarousel(wrapper.current, {
       Autoplay: {
         timeout: 3000,
       },
@@ -29,25 +27,25 @@ function ReactCarousel(props) {
     };
   }, [props.items, props.options]);
 
-  return <div className={`carousel ${props.class || ""}`} ref={wrapper}>
- {props.items.map((item) => {
-          return (
-            <li
-              key={item.id}
-              className={`carousel__slide slider__small-item w-150`}
-            >
-              <img
-                className="carousel__img"
-                src={item.img}
-                alt={`Slider ${item.tag}`}
-              />
-            </li>
-          );
-        })}
-            
-
-           
-  </div>;
+  return (
+    <div className={`carousel ${props.class || ''}`} ref={wrapper}>
+      {props.items.map(item => {
+        return (
+          <li
+            key={item.id}           
+            className={`carousel__slide slider__small-item w-150 slide_${item.id}`}
+          >
+             <p className={`page_about page_about_${item.id}`}>{`${item.tag}`}</p>
+            {/* <img
+              className="carousel__img"
+              src={item.img}
+              alt={`Slider ${item.tag}`}
+            /> */}
+          </li>
+        );
+      })}
+    </div>
+  );
 }
 
 export default ReactCarousel;
