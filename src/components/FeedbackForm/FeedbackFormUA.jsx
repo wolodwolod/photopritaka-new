c0dd35a024df57965b38e73070d24040711d5489
Volodymyr Bendyk <92178622+wolodwolod@users.noreply.github.com>
2023-05-09 08:33:37 +0300

    Update FeedbackFormUA.jsx

--- src/components/FeedbackForm/FeedbackFormUA.jsx
+++ src/components/FeedbackForm/FeedbackFormUA.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 
 import Loader from 'components/Loader';
 
-import * as text from './feedback_form_text';
+// import * as text from './feedback_form_text';
 
 import {
   MessageSuccessSendUA,
